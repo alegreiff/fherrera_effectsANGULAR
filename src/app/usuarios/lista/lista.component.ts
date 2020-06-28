bc94de1dda1d7eca459595af488d8683ce77242b
Jaime de Greiff <alegreiff@gmail.com>
2020-06-28 16:02:54 -0500

    Primer COMMIT

---
+++ src/app/usuarios/lista/lista.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { Usuario } from 'src/app/models/usuario.model';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styles: [
+  ]
+})
+export class ListaComponent implements OnInit {
+
+  usuarios: Usuario[] = []
+  constructor(
+    private userSERVICE: UsuarioService
+  ) { }
+
+  ngOnInit() {
+    this.userSERVICE.getUsers()
+    .subscribe( users => {
+      console.log(users);
+      this.usuarios = users
+    })
+  }
+
+}
